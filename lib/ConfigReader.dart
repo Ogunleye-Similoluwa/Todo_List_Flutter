be55816cc324a2bc2921bd60314c4a5dd803f73e
Similoluwa <osimiloluwa9@gmail.com>
2024-02-27 13:09:08 +0100

    new

---
+++ lib/ConfigReader.dart
@@ -0,0 +1,21 @@
+
+
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class ConfigReader{
+ late final Map<String, dynamic> _config ;
+
+  Future<void> initialize() async{
+    final configString = await rootBundle.loadString("config.app_config.json");
+    _config = json.decode(configString);
+  }
+
+  String getSecretKey(){
+    return _config["secret_key"];
+  }
+
+
+
+}
